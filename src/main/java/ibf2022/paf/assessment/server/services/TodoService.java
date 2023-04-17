ac9e53eba54bf960f075a9c32a808df90fc15c2f
Codetatorang <102163869+Codetatorang@users.noreply.github.com>
2023-04-17 09:40:37 +0800

    adjusted to rollback user

--- src/main/java/ibf2022/paf/assessment/server/services/TodoService.java
+++ src/main/java/ibf2022/paf/assessment/server/services/TodoService.java
@@ -23,31 +23,30 @@ public class TodoService {
     UserRepository userRepo;
 
     @Transactional(rollbackFor = UserException.class)
-    public Boolean upsertTask(List<Task> taskList, String userName) throws UserException{
+    public Boolean upsertTask(List<Task> taskList, String userName) throws UserException {
         Logger logger = LoggerFactory.getLogger(TodoService.class);
-        
+
         User update_User = new User();
-
-        if(userRepo.findUserByUsername(userName).isEmpty()){
-            //create a new user if not exists
-            User new_user = new User();
-            new_user.setName(userName.substring(0,1).toUpperCase().substring(1));
-            new_user.setUsername(userName);
-
-            //generate uuid and add user to database
-            String uuid = userRepo.insertUser(new_user);
-            new_user.setUserId(uuid);
-
-            update_User = new_user;
-        }else{
-            update_User = userRepo.findUserByUsername(userName).get();
-        }
-
-
-        //update tasks into database
         try {
+            if (userRepo.findUserByUsername(userName).isEmpty()) {
+                // create a new user if not exists
+                User new_user = new User();
+                new_user.setName(userName.substring(0, 1).toUpperCase().substring(1));
+                new_user.setUsername(userName);
+
+                // generate uuid and add user to database
+                String uuid = userRepo.insertUser(new_user);
+                new_user.setUserId(uuid);
+
+                update_User = new_user;
+            } else {
+                update_User = userRepo.findUserByUsername(userName).get();
+            }
+
+            // update tasks into database
+
             logger.info("user's user ID: %s".formatted(update_User.getUserId()));
-            for(Task task : taskList){
+            for (Task task : taskList) {
                 taskRepo.insertTask(task, update_User);
             }
         } catch (Exception e) {
